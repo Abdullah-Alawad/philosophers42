16b6d6d6394abb5895499af67cafb3e1714bc5fc
Abdullah Alawad <abdullahalawad123321@gmail.com>
2025-02-23 23:01:15 +0300

    more understanding of threads

--- discover.c
+++ discover.c
@@ -5,6 +5,19 @@
 
 int sum = 0;
 
+void    *runner2(void *param)
+{
+    int *x = (int *)param;
+    (*x)++;
+    printf("value of x: %d\n", *x);
+}
+
+void    *runner3(void *param)
+{
+    int *x = (int *)param;
+    printf("value of x here: %d\n", *x);
+}
+
 void    *runner(void *param)
 {
     int i;
@@ -26,10 +39,13 @@ int main(int argc, char **argv)
     pthread_t tid1;
     pthread_t tid2;
     pthread_attr_t attr;
+    int x = 5;
 
     pthread_attr_init(&attr);
-    pthread_create(&tid1, &attr, runner, argv[1]);
-    pthread_create(&tid2, &attr, runner, argv[1]);
+    // pthread_create(&tid1, &attr, runner, argv[1]);
+    // pthread_create(&tid2, &attr, runner, argv[1]);
+    pthread_create(&tid1, &attr, runner2, &x);
+    pthread_create(&tid2, &attr, runner3, &x);
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
 
