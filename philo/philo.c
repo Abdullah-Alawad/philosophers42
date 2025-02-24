45c964ccd276a88e7097b3a09ad45536842dd2df
Abdullah Alawad <abdullahalawad123321@gmail.com>
2025-02-24 20:59:57 +0300

    started project, set header file(structures), started doing the setter functions

--- philo/philo.c
+++ philo/philo.c
@@ -1,9 +1,59 @@
-#include "philo.c"
+#include "philo.h"
+
+int	init_forks(t_table *table)
+{
+	int	i;
+
+	table->forks = (pthread_mutex_t *)malloc(sizeof(pthead_mutex_t)
+		* table->philos_num);
+	if (!table->forks)
+		return (0);
+	i = 0;
+	while (i < table->philos_num)
+	{
+		if (pthread_mutex_init(&table->forks[i], NULL) != 0)
+			return (0);
+		i++;
+	}	
+}
+
+int	init_table(t_table *table, int ac, char **av)
+{
+	struct timeval	tv;
+
+	table->philos_num = ft_atoi(av[1]);
+	table->time_to_die = ft_atoi(av[2]);
+	table->time_to_eat = ft_atoi(av[3]);
+	table->time_to_sleep = ft_atoi(av[4]);
+	table->stop_simulation = 0;
+	if (table->philos_num < 2 || table->time_to_die < 0
+		|| table->time_to_eat < 0 || table->time_to_sleep < 0)
+		return (0);
+	if (gettimeofday(&tv, NULL) == 0)
+		table->start_time = tv.tv_sec * 1000LL + tv.tv_usec / 1000;
+	else
+		return (0);
+	if (ac == 6)
+	{
+		if (ft_atoi(av[5]) < 0)
+			return (0);
+		table->meals_num = ft_atoi(av[5]);
+	}
+	else
+		table->meals_num = 0;
+	if (!init_forks(table))
+		return (0);
+	init_philos(table);
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	//main is ready
-	printf("hello?");
+	t_table	table;
+
+	if (argc < 5 || argc > 6)
+		return (1);
+	if (!init_table(&table, argc, argv))
+		return (1);
+	
 }
--- philo/philo.h
+++ philo/philo.h
@@ -2,5 +2,37 @@
 # define PHILO_C
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>  // memset
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_table
+{
+	struct s_philo	*philos;
+	int				philos_num;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				meals_num;
+	int				stop_simulation;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_lock;
+	pthread_mutex_t	sim_lock;
+	long long		start_time;
+}	t_table;
+
+
+typedef struct s_philo
+{
+    pthread_t		thread;
+    int				id;
+    long long		last_eat;
+    pthread_mutex_t *left_f;
+    pthread_mutex_t *right_f;
+	struct s_table	*table;
+}	t_philo;
+
 
 #endif
