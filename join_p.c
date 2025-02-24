afd5ddaa54b87b8fa0357e809c3c7522706afd66
Abdullah Alawad <abdullahalawad123321@gmail.com>
2025-02-24 15:37:12 +0300

    pthread_join return value practice

--- join_p.c
+++ join_p.c
@@ -3,28 +3,26 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void	*roll(void *result)
+void	*roll()
 {
 	int res = (rand() % 6) + 1;
-	//int *result = malloc(sizeof(int));
+	int *result = malloc(sizeof(int));
 	*result = res;
-	printf("dice res: %d\n", res);
-	//printf("add res: %p\n", result);
+	//printf("dice res: %d\n", res);
+	printf("add res: %p\n", result);
 	return ((void *)result);
 }
 
 int main(void)
 {
-	pthread_t th[5];
-	int *res = malloc(sizeof(int) * 5);
-	int i;
+	pthread_t th;
+	int *res;
 	srand(time(NULL));
-	for (i = 0; i < 5; i++)
-		pthread_create(&th[i], NULL, &roll, NULL);
 
-	for (i = 0; i < 5; i++)
-		pthread_join(th[i], (void**) &res[i]);
-	for(i = 0; i < 5; i++)
-		printf("in main dice: %d\n", res[i]);
+	pthread_create(&th, NULL, &roll, NULL);
 
+	pthread_join(th, (void **) &res);
+	printf("result in main: %d\n", *res);
+	printf("add in main: %p\n", res);
+	free(res);
 }
