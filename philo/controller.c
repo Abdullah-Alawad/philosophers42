d019d94355dcbba043da915f9a7d2bca1d9d6e05
Abdallah Alawad <aalawad@c1r2s6.42amman.com>
2025-03-13 14:59:07 +0300

    code norminetted fully

--- philo/controller.c
+++ philo/controller.c
@@ -12,6 +12,32 @@
 
 #include "philo.h"
 
+int	check_death(t_table *table, int i)
+{
+	if (get_t_in_ms() - table->philos[i].last_eat >= table->time_to_die)
+	{
+		pthread_mutex_lock(&table->print_lock);
+		print_status(&table->philos[i], "died", RED);
+		table->stop_simulation = 1;
+		pthread_mutex_unlock(&table->print_lock);
+		pthread_mutex_unlock(&table->sim_lock);
+		return (1);
+	}
+	return (0);
+}
+
+int	check_num_of_meals(t_table *table, int all_philos)
+{
+	if (all_philos == table->philos_num)
+	{
+		pthread_mutex_lock(&table->sim_lock);
+		table->stop_simulation = 1;
+		pthread_mutex_unlock(&table->sim_lock);
+		return (1);
+	}
+	return (0);
+}
+
 void	*control_death(void *arg)
 {
 	t_table	*table;
@@ -26,28 +52,16 @@ void	*control_death(void *arg)
 		while (i < table->philos_num)
 		{
 			pthread_mutex_lock(&table->sim_lock);
-			if (get_t_in_ms() - table->philos[i].last_eat >= table->time_to_die)
-			{
-				pthread_mutex_lock(&table->print_lock);
-				print_status(&table->philos[i], "died", RED);
-				table->stop_simulation = 1;
-				pthread_mutex_unlock(&table->print_lock);
-				pthread_mutex_unlock(&table->sim_lock);
+			if (check_death(table, i))
 				return (NULL);
-			}
 			if (table->meals_required > 0
 				&& table->philos[i].meals_eaten >= table->meals_required)
 				all_philos++;
 			pthread_mutex_unlock(&table->sim_lock);
 			i++;
 		}
-		if (all_philos == table->philos_num)
-		{
-			pthread_mutex_lock(&table->sim_lock);
-			table->stop_simulation = 1;
-			pthread_mutex_unlock(&table->sim_lock);
+		if (check_num_of_meals(table, all_philos))
 			return (NULL);
-		}
 		usleep(200);
 	}
 	return (NULL);
